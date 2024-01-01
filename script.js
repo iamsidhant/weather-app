c32084a1ec910f09c7256967d8b0576a5743cc12
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-01 13:49:11 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const apikey = "";
+
+const weatherDataEl = document.getElementById("weather-data");
+
+const cityInputEl = document.getElementById("city-input");
+
+const formEl = document.querySelector("form");
