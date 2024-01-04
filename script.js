48eb979f5f8c4dda7b93e1237bde1e688b50532a
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-04 12:59:33 +0530

    moving forward

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ const weatherDataEl = document.getElementById("weather-data");
 const cityInputEl = document.getElementById("city-input");
 
 const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const cityValue = cityInputEl.value;
+    getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue){
+    try {
+        const response = await fetch();
+
+        if (!response.ok) {
+            throw new Error("Network reponse was not ok");
+        }
+    } catch (error) {
+        weatherDataEl.querySelector(".icon").innerHTML = "";
+        weatherDataEl.querySelector(".temperature").textContent = "";
+        weatherDataEl.querySelector(".description") = 
+        "An error happened, please try later";
+
+        weatherDataEl.querySelector(".details").innerHTML = "";
+    }
+}
